ea12a8beda4c8267d7294566b6bc64601276e4f2
moxel-spec <maksmerekzo@gmail.com>
2022-03-31 16:03:56 +0300

    lector about finding elements by Get,Find,Eq

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" /> 
+
+
+// it('By ID', () => {
+//     cy.visit("https://facebook.com/")
+//     cy.get('#email')
+// })
+
+
+// it('By Class', () => {
+//     cy.visit("https://docs.cypress.io/api/commands/get#Timeouts")
+//     cy.get('.DocSearch.DocSearch-Button').click()
+// })
+
+
+// it('By Tag value', () => {
+//     cy.visit("https://www.pitcher.com/")
+//     cy.get('[id="main-nav"]')
+// })
+
+
+
+it.only ('Using get with find and eq', () => {
+    cy.viewport(1800,700)
+    cy.visit("https://docs.cypress.io/api/commands/get#Syntax")
+    cy.get('div').find('nav').find('ul').find('li').find('a').eq(1)
+})
