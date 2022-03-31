5b36c2d29ec567a195524667d51624d0afa1859f
moxel-spec <maksmerekzo@gmail.com>
2022-03-31 17:28:10 +0300

    add 2 lecture code

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,22 +1,23 @@
 /// <reference types="cypress" /> 
 
 
-// it('By ID', () => {
-//     cy.visit("https://facebook.com/")
-//     cy.get('#email')
-// })
-
-// New test comment
-// it('By Class', () => {
-//     cy.visit("https://docs.cypress.io/api/commands/get#Timeouts")
-//     cy.get('.DocSearch.DocSearch-Button').click()
-// })
+it('By ID', () => {
+    cy.visit("https://facebook.com/")
+    cy.get('#email')
+})
 
 
-// it('By Tag value', () => {
-//     cy.visit("https://www.pitcher.com/")
-//     cy.get('[id="main-nav"]')
-// })
+
+it('By Class', () => {
+    cy.visit("https://docs.cypress.io/api/commands/get#Timeouts")
+    cy.get('.DocSearch.DocSearch-Button').click()
+})
+
+
+it('By Tag value', () => {
+    cy.visit("https://www.pitcher.com/")
+    cy.get('[id="main-nav"]')
+})
 
 
 
